8ed3c6150732d88c93a7962517ac5113464b4908
Александр Меркулов <merkoolow@Aleksandrs-MacBook-Pro.local>
2022-10-05 16:05:44 +0300

    completed

--- index.cpp
+++ index.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
+#include <cmath>
+//pow() возведение к степени
 
 int operations(int num1, int num2) {
-	return 0;
+	int division = num1 % num2;
+	return pow(division, 4);
 }
